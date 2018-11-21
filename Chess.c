6d4040e725216e43bcbeffb05836576db535e8e1
Hb-key <44340302+Hb-key@users.noreply.github.com>
2018-11-21 12:45:37 +0800

    Add files via upload

--- Chess.c
+++ Chess.c
@@ -1,30 +1,4 @@
 #include"Chess.h"
-void menu()
-{
-	printf("***************************\n");
-	printf("****   Welcome Chess   ****\n");
-	printf("***************************\n");
-	printf("****  1.play   0.exit  ****\n");
-	printf("***************************\n");
-}
-
-int main()
-{
-	int input = 0;
-	scanf("&d", &input);
-	switch (input)
-	{
-	case 1:
-		Game();
-		break;
-	case 0:
-		printf("Exit!!!\n");
-		break;
-	default:
-		printf("Please enter again:\n");
-		break;
-	}
-}
 
 void Init_board(char board[ROW][COL], int row, int col)
 {
@@ -33,8 +7,7 @@ void Init_board(char board[ROW][COL], int row, int col)
 
 void Display_board(char board[ROW][COL], int row, int col)
 {
-	int i = 0;
-	for (i = 0; i<row; i++)
+	for (int i = 0; i<row; i++)
 	{
 		printf(" %c | %c | %c \n", board[i][0], board[i][1], board[i][2]);
 		if (i != row)
@@ -43,4 +16,62 @@ void Display_board(char board[ROW][COL], int row, int col)
 
 		}
 	}
+}
+
+void Player_move(char board[ROW][COL], int row, int col)
+{
+	int x = 0;
+	int y = 0;
+	printf("玩家走棋:\n");
+	while (1)
+	{
+		printf("请输入坐标：");
+		scanf("%d%d", &x, &y);
+		if (x > 0 && x < row && y>0 && y < col)
+		{
+			if (board[x - 1][y - 1] == ' ')
+			{
+				board[x - 1][y - 1] = 'X';
+				break;
+			}
+			else
+			{
+				printf("此坐标已有值！\n");
+			}
+		}
+		else
+		{
+			printf("输入的坐标不存在！\n");
+		}
+	}
+}
+
+void Computer_move(char board[ROW][COL], int row, int col)
+{
+	int x = 0;
+	int y = 0;
+	printf("电脑走棋：");
+	while (1)
+	{
+		x = rand() % row;
+		y = rand() % col;
+		if (board[x][y] == ' ')
+		{
+			board[x][y] = '0';
+			break;
+		}
+	}
+}
+
+int Check_full(char board[COL][ROW], int row, int col)
+{
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < col; j++)
+		{
+			if (board[i][j] == ' ')
+				return 0;
+		}
+	}
+	return 1;
 }
