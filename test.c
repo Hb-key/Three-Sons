fa86e4f81672aecccefdfccfbfdfa2ba92b93bd1
Hb-key <44340302+Hb-key@users.noreply.github.com>
2018-11-22 22:40:45 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,12 +1,40 @@
-#include<stdio.h>
+#include"Chess.h"
+
+void menu()
+{
+	printf("***************************\n");
+	printf("****   Welcome Chess   ****\n");
+	printf("***************************\n");
+	printf("****  1.play   0.exit  ****\n");
+	printf("***************************\n");
+}
+
+
+void test()
+{
+	int input = 0;
+	scanf("&d", &input);
+	do
+	{
+		menu();
+		switch (input)
+		{
+		case 1:
+			Game();
+			break;
+		case 0:
+			printf("Exit!!!\n");
+			break;
+		default:
+			printf("Please enter again:\n");
+			break;
+		}
+
+	} while (input);
+}
+
 int main()
 {
-	for (int i = 0; i < 3; i++)
-	{ 
-		for (int j = 0; j < i; j++)
-		{
-			printf("  |  |  \n");
-		}
-		printf("________\n");
-	}	返回 0 ;
+	test();
+	return 0;
 }
